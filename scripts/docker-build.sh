74e6860977186b23e8344d7b1ccb062688c4e03b
Dan Philibin <dan@cornerhealth.com>
2025-12-23 13:51:57 +0000

    update docker build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
-docker buildx create --use
+
+# Create/use a builder that supports multi-platform
+docker buildx create --name multiarch --driver docker-container --use 2>/dev/null || docker buildx use multiarch
+
 pkgV=`node -e "console.log(require('./package.json').version.trim())"`
 specificVersionTag=cornerhealth/interval-server:$pkgV
-# Build the docker image + pushes it to registry
-docker buildx build --output=type=registry --platform=linux/amd64,linux/arm64 -t cornerhealth/interval-server:latest -t $specificVersionTag .
+
+# Build the docker image + push to registry (multi-platform)
+docker buildx build --push --platform linux/amd64,linux/arm64 \
+  -t cornerhealth/interval-server:latest \
+  -t $specificVersionTag .
